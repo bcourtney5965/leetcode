712b24c3ae509057675553f592f8556d47c0656a
Brandon Courtney <brandon.h.courtney@gmail.com>
2024-02-08 20:58:33 -0700

    Two pointer approach

--- removeDuplicatesFromSortedArrayII/index.ts
+++ removeDuplicatesFromSortedArrayII/index.ts
@@ -1,50 +1,33 @@
-// run time 70ms & 57ms
-// run time 61ms & 105ms 79
-type NullableArray = (number | null)[];
-
-function updateElements(
-  nums: NullableArray,
-  position: number,
-  count: number,
-): void {
-  // remove duplicated elements in excess of 2
-  nums.splice(position - (count - 2), count - 2);
-}
-
-const appendNull = (nums: NullableArray, duplicateCount: number): void => {
-  // for each element duplicated in excess of 2
-  for (; duplicateCount > 2; duplicateCount--) {
-    // append a null to the end
-    nums.push(null);
-  }
-};
-
 function removeDuplicates(nums: number[]): number {
-  // tracks elements of same value
-  let duplicateCount: number = 1;
-  // loop over
-  for (let i = 0; i < nums.length; i++) {
-    // after the first element
-    if (i > 0) {
-      // if current char is same as previous one
-      if (nums[i] === nums[i - 1]) {
-        //increment duplicateCount counter
-        duplicateCount++;
-      }
-      // if we need to check preceeding elements for sameness
-      // basically if the current element is different from the previous or its the final character
-      if (nums[i] !== nums[i - 1] || i === nums.length - 1) {
-        // if the duplicateCount flag is greater that 2
-        if (duplicateCount > 2) {
-          // update the necessary previous positions
-          updateElements(nums, i, duplicateCount);
-          // append necessary null's at end
-          appendNull(nums, duplicateCount);
-        }
-        // reset duplicateCount
-        duplicateCount = 1;
-      }
+  // instantiate count variable
+  let count: number = 1;
+  // instantiate index variable
+  let index: number = 1;
+  // loop over nums
+  for (let i = 1; i < nums.length; i++) {
+    // if current element is different from previous
+    if (nums[i] === nums[i - 1]) {
+      count++;
+      // if current element IS same as previous
+    } else if (nums[i] !== nums[i - 1]) {
+      // reset count
+      count = 1;
+    }
+
+    // if count is less than or equal to 2
+    if (count <= 2) {
+      nums[index] = nums[i];
+      index++;
     }
   }
-  return nums.reduce((acc, el, i) => (el === null ? acc : i + 1));
+  return index;
 }
+
+console.log(
+  'scalloped potatoes::: removeDuplicates([1,1,1,2,2,3])',
+  removeDuplicates([1, 1, 1, 2, 2, 3]),
+);
+// console.log(
+//   'scalloped potatoes::: removeDuplicates([0,0,1,1,1,1,2,3,3])',
+//   removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]),
+// );
